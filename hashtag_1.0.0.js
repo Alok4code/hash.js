bdbb66db50843c3cdadaec3e7a6c51befdbfbcfb
Raghav Yadav <79745774+raghav9official@users.noreply.github.com>
2021-05-10 19:46:17 +0530

    Update hashtag_1.0.0.js

--- hashtag_1.0.0.js
+++ hashtag_1.0.0.js
@@ -62,8 +62,21 @@ const self = {
          style.type = "text/css";
          style.href = url;
          head.appendChild(style);
-    }
-
+    },
+    ajax: (url, method) => {
+           if(method == null){console.log("Ajax request cannot be performed while method is null.")}else{
+           var xhttp = new XMLHttpRequest();
+           xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                self.element.innerHTML =
+                this.responseText;
+            }
+           };
+           xhttp.open(method, url, true);
+           xhttp.send();
+         }
+    },
+    
 
 
 
