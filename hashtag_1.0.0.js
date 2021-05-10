6ce1d61fe24710583b9142328d2c27131b6716db
Raghav Yadav <79745774+raghav9official@users.noreply.github.com>
2021-05-10 19:04:22 +0530

    Addded features

--- hashtag_1.0.0.js
+++ hashtag_1.0.0.js
@@ -1,12 +1,38 @@
 function #(sel)
 {
-const self=
-     {
-       elm: document.querySelector(sel),
-       html: self.elm,
-       on(event, callback) = {
-                 self.elm.addEventListener(event, callback)
-               }
-     }
+const self = {
+    element: document.querySelector(selector),
+    html: ()=> self.element,
+    on: (event, callback) => {
+      self.element.addEventListener(event, callback)
+    },
+    hide: ()=> {
+      self.element.style.display = 'none'
+    },
+    attr: (name, value) => {
+      if(value == null)
+        return self.element.getAttribute(name)
+      else
+        self.element.setAttribute(name, value)
+    },
+    show: () => {
+        self.element.style.display = "" 
+    },
+    append: (text) => {
+        self.element.innerHTML = self.element.innerHTML + text
+    },
+    prepend: (text) => {
+        self.element.innerHTML = text + self.element.innerHTML
+    },
+    
+
+
+
+
+
+
+
+
+  }
 return self;
 }
