3884e1a40db91934e9f2e9256522e93655894f36
Raghav Yadav <79745774+raghav9official@users.noreply.github.com>
2021-05-17 00:12:51 +0530

    Update hashtag_1.0.1.js

--- hashtag_1.0.1.js
+++ hashtag_1.0.1.js
@@ -179,31 +179,39 @@ function hash(sel) {
      },
      vibrate: function(val){
          navigator.vibrate(val);
-     },
-     speech: function(elm1, elm2){
-            var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
-            var recognition = new SpeechRecognition();
-            recognition.onstart = function() {};
- 
-            recognition.onspeechend = function() {
-                recognition.stop();
-            }
- 
-            // This runs when the speech recognition service returns result
-            recognition.onresult = function(event, elm1, elm2) {
-                var transcript = event.results[0][0].transcript;
-                var confidence = event.results[0][0].confidence;
-                document.querySelector(".title").innerHTML = transcript;
-            };
- 
-            // start recognition
-            recognition.start();
      }
-
-
-
-
-
   };
   return self;
 }
+
+
+function runSpeech() {
+		        // get output div reference
+		        var output = document.getElementById("title");
+		        // get action element reference
+		        var action = document.getElementById("body");
+                // new speech recognition object
+                var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+                var recognition = new SpeechRecognition();
+            
+                // This runs when the speech recognition service starts
+                recognition.onstart = function() {
+                    action.innerHTML = "<small>listening, please speak...</small>";
+                };
+                
+                recognition.onspeechend = function() {
+                    action.innerHTML = "<small>stopped listening, hope you are done...</small>";
+                    recognition.stop();
+                }
+              
+                // This runs when the speech recognition service returns result
+                recognition.onresult = function(event) {
+                    var transcript = event.results[0][0].transcript;
+                    var confidence = event.results[0][0].confidence;
+                    output.innerHTML = "<b>Text:</b> " + transcript + "<br/> <b>Confidence:</b> " + confidence*100+"%";
+                    output.classList.remove("hide");
+                };
+              
+                 // start recognition
+                 recognition.start();
+	        }
