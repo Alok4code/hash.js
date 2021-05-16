6f2248664adec0080b4b714485671e4251f8564a
Raghav Yadav <79745774+raghav9official@users.noreply.github.com>
2021-05-16 22:53:01 +0530

    Update hashtag_1.0.1.js

--- hashtag_1.0.1.js
+++ hashtag_1.0.1.js
@@ -198,6 +198,40 @@ function hash(sel) {
             
             var rellax = new Rellax(elm, options);
 
+     },
+     speech: function (){
+         var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+         var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList;
+         var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent;
+         var recognition = new SpeechRecognition();
+         var speechRecognitionList = new SpeechGrammarList();
+         speechRecognitionList.addFromString(grammar, 1);
+         recognition.grammars = speechRecognitionList;
+         recognition.continuous = false;
+         recognition.lang = 'en-US';
+         recognition.interimResults = false;
+         recognition.maxAlternatives = 1;
+           self.element.onclick = function(){
+                   recognition.start();
+                   console.log('Ready to receive a color command.');
+           }
+           recognition.onresult = function(event) {
+                   return event.results[0][0].transcript;
+           }
+           recognition.onspeechend = function() {
+                   recognition.stop();
+           }
+
+           recognition.onnomatch = function(event) {
+                   return false;
+           }
+
+           recognition.onerror = function(event) {
+                  return "ERROR:" + event.error;
+           }
+
+
+
      }
 
 
