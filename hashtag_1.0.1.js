507c4a28079a39c4203c43a38ff69cb54f880ce5
Raghav Yadav <79745774+raghav9official@users.noreply.github.com>
2021-05-17 12:13:38 +0530

    Update hashtag_1.0.1.js

--- hashtag_1.0.1.js
+++ hashtag_1.0.1.js
@@ -184,34 +184,14 @@ function hash(sel) {
   return self;
 }
 
+function read(message) {
+  var speech = new SpeechSynthesisUtterance();
 
-function runSpeech() {
-		        // get output div reference
-		        var output = document.querySelector("title");
-		        // get action element reference
-		        var action = document.querySelector("body");
-                // new speech recognition object
-                var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
-                var recognition = new SpeechRecognition();
-            
-                // This runs when the speech recognition service starts
-                recognition.onstart = function() {
-                    action.innerHTML = "<small>listening, please speak...</small>";
-                };
-                
-                recognition.onspeechend = function() {
-                    action.innerHTML = "<small>stopped listening, hope you are done...</small>";
-                    recognition.stop();
-                }
-              
-                // This runs when the speech recognition service returns result
-                recognition.onresult = function(event) {
-                    var transcript = event.results[0][0].transcript;
-                    var confidence = event.results[0][0].confidence;
-                    output.innerHTML = "<b>Text:</b> " + transcript + "<br/> <b>Confidence:</b> " + confidence*100+"%";
-                    output.classList.remove("hide");
-                };
-              
-                 // start recognition
-                 recognition.start();
-	        }
+  // Set the text and voice attributes.
+  speech.text = message;
+  speech.volume = 1;
+  speech.rate = 1;
+  speech.pitch = 1;
+
+  window.speechSynthesis.speak(speech);
+}
