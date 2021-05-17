7bba241707b4309fcbbec2b00b75150ecbd2f04f
Raghav Yadav <79745774+raghav9official@users.noreply.github.com>
2021-05-17 13:20:28 +0530

    Update hashtag_1.0.1.js

--- hashtag_1.0.1.js
+++ hashtag_1.0.1.js
@@ -186,8 +186,8 @@ function hash(sel) {
          var speech = new SpeechSynthesisUtterance();
          speech.text = message;
          speech.volume = 1;
-         speech.rate = 1;
-         speech.pitch = 1;
+         speech.rate = 0;
+         speech.pitch = -1;
          window.speechSynthesis.speak(speech);
     }
 
